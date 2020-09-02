3d6cc897f9444b81b9abbe9090389b3ee8e17066
agusrichard <agus.richard21@gmail.com>
2020-09-02 07:51:25 +0700

    Learn Pygame

--- app.js
+++
@@ -1,8 +0,0 @@
-const fibonacci = require('./src/fibonacci');
-const dataPribadi = require('./src/arr');
-const { isUsernameValid, isPasswordValid } = require('./src/validate');
-
-console.log(fibonacci(5));
-console.log(dataPribadi());
-console.log(isUsernameValid('agusric'));
-console.log(isPasswordValid('ThisIsAPassword!999'));
--- src/arr.js
+++
@@ -1,9 +0,0 @@
-function dataPribadi() {
-  return {
-    nama: 'Agus Richard Lubis',
-    nomor_ponsel: '085710276393',
-    email: 'agus.richard21@gmail.com'
-  }
-}
-
-module.exports = dataPribadi;
